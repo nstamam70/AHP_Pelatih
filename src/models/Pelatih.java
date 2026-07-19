6a1705af2e1b920a4149db3002ff927eeb7685eb
Paiii18 <ahmadrivai.fun@gmail.com>
2026-07-19 18:54:11 +0700

    feat: add model & logic model pelatih

---
+++ src/models/Pelatih.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Pelatih {
+
+    private int idPelatih;
+    private String kodePelatih;
+    private String namaPelatih;
+    private String alamat;
+    private String noTelp;
+    private String lisensi;
+    private String status;
+
+    public int getIdPelatih() { return idPelatih; }
+    public void setIdPelatih(int idPelatih) { this.idPelatih = idPelatih; }
+
+    public String getKodePelatih() { return kodePelatih; }
+    public void setKodePelatih(String kodePelatih) { this.kodePelatih = kodePelatih; }
+
+    public String getNamaPelatih() { return namaPelatih; }
+    public void setNamaPelatih(String namaPelatih) { this.namaPelatih = namaPelatih; }
+
+    public String getAlamat() { return alamat; }
+    public void setAlamat(String alamat) { this.alamat = alamat; }
+
+    public String getNoTelp() { return noTelp; }
+    public void setNoTelp(String noTelp) { this.noTelp = noTelp; }
+
+    public String getLisensi() { return lisensi; }
+    public void setLisensi(String lisensi) { this.lisensi = lisensi; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
